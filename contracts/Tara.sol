5890151fbfb550f84f99c4b486ba9985b1f9cb95
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-02-20 20:37:11 +0200

    feat(token): adds pausable functionality to token

--- contracts/Tara.sol
+++ contracts/Tara.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.6.0 <0.8.0;
 
-import "@openzeppelin/contracts/presets/ERC20PresetFixedSupply.sol";
+import "./ERC20Pauser.sol";
 
-contract Tara is ERC20PresetFixedSupply {
-    constructor() ERC20PresetFixedSupply("Taraxa Coin", "TARA", 1000000000, msg.sender) {}
+contract Tara is ERC20Pauser {
+    constructor() ERC20Pauser("Taraxa Coin", "TARA") {
+        _mint(_msgSender(), 1000000000);
+    }
 }
