63db4bcdc91cc9a05417ab4d5868905906b92d6b
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-02-26 17:46:26 +0200

    feat(tests): record gas used

--- test/Claim.test.js
+++ test/Claim.test.js
@@ -10,6 +10,28 @@ require("chai")
   .use(require("chai-bignumber")(BigNumber))
   .should();
 
+let gasUsedRecords = [];
+let gasUsedTotal = 0
+
+function recordGasUsed(tx, label) {
+  gasUsedTotal += tx.receipt.gasUsed;
+  gasUsedRecords.push(
+    String(label + " | GasUsed: " + tx.receipt.gasUsed).padStart(80)
+  );
+}
+
+function printGasUsed() {
+  console.group('Gas used');
+  console.log("-".repeat(80));
+  for (let i = 0; i < gasUsedRecords.length; ++i) {
+    console.log(gasUsedRecords[i]);
+  }
+  console.log(String("Total: " + gasUsedTotal).padStart(80));
+  console.log("-".repeat(80));
+  console.groupEnd();
+}
+
+
 contract("Claim", function (accounts) {
   const walletAddress = accounts[0],
     trustedAddress = accounts[1],
@@ -30,6 +52,10 @@ contract("Claim", function (accounts) {
     await this.token.approve(this.contract.address, balance);
   });
 
+  after(async () => {
+    printGasUsed();
+  });
+
   it("transfers tokens if the signature is valid", async function () {
     const encoded = abi.soliditySHA3(
       ["address", "uint", "uint"],
@@ -39,7 +65,8 @@ contract("Claim", function (accounts) {
     const { v, r, s } = ethUtil.ecsign(encoded, privateKey);
     const hash = ethUtil.toRpcSig(v, r, s);
 
-    await this.contract.claim(clientAddress, balance, nonce, hash);
+    const tx = await this.contract.claim(clientAddress, balance, nonce, hash);
+    recordGasUsed(tx, "claim - transfers tokens if the signature is valid");
 
     const newBalance = await this.token.balanceOf(clientAddress);
     newBalance
@@ -56,9 +83,10 @@ contract("Claim", function (accounts) {
     const { v, r, s } = ethUtil.ecsign(encoded, privateKey);
     const hash = ethUtil.toRpcSig(v, r, s);
 
-    await this.contract.claim(clientAddress, balance, nonce, hash, {
+    const tx = await this.contract.claim(clientAddress, balance, nonce, hash, {
       from: otherAddress,
     });
+    recordGasUsed(tx, "claim - can claim tokens for other party if signature is valid");
 
     const newBalance = await this.token.balanceOf(clientAddress);
     newBalance
@@ -78,7 +106,8 @@ contract("Claim", function (accounts) {
     );
     const { v: v1, r: r1, s: s1 } = ethUtil.ecsign(encoded1, privateKey);
     const hash1 = ethUtil.toRpcSig(v1, r1, s1);
-    await this.contract.claim(clientAddress, balance, 1, hash1);
+    const tx1 = await this.contract.claim(clientAddress, balance, 1, hash1);
+    recordGasUsed(tx1, "claim - can claim multiple times with different signatures - 1");
 
     const encoded2 = abi.soliditySHA3(
       ["address", "uint", "uint"],
@@ -86,7 +115,8 @@ contract("Claim", function (accounts) {
     );
     const { v: v2, r: r2, s: s2 } = ethUtil.ecsign(encoded2, privateKey);
     const hash2 = ethUtil.toRpcSig(v2, r2, s2);
-    await this.contract.claim(clientAddress, balance, 2, hash2);
+    const tx2 = await this.contract.claim(clientAddress, balance, 2, hash2);
+    recordGasUsed(tx2, "claim - can claim multiple times with different signatures - 2");
 
     const newBalance = await this.token.balanceOf(clientAddress);
     newBalance
@@ -118,7 +148,8 @@ contract("Claim", function (accounts) {
     const { v, r, s } = ethUtil.ecsign(encoded, privateKey);
     const hash = ethUtil.toRpcSig(v, r, s);
 
-    this.contract.claim(clientAddress, balance, nonce, hash);
+    const tx = await this.contract.claim(clientAddress, balance, nonce, hash);
+    recordGasUsed(tx, "claim - doesn't transfer tokens if already claimed");
     await truffleAssert.reverts(
       this.contract.claim(clientAddress, balance, nonce, hash),
       "Claim: Already claimed"
@@ -167,7 +198,8 @@ contract("Claim", function (accounts) {
       .should.be.equal(0, "The balance should be 0");
 
     await this.token.approve(this.contract.address, balance);
-    await this.contract.claim(clientAddress, balance, nonce, hash)
+    const tx = await this.contract.claim(clientAddress, balance, nonce, hash);
+    recordGasUsed(tx, "claim - it reverts the transaction if the transfer fails");
 
     const newBalance = await this.contract.getClaimedAmount(clientAddress, balance, nonce);
     newBalance
