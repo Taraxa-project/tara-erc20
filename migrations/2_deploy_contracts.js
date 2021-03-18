f9bfda479bd6e4bcb9b6a4511548ab41a13327ba
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-03-18 19:58:28 +0200

    build(truffle-config): adds taraxa config && changes the number of minted tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
-const Tara = artifacts.require("./Tara.sol");
-const Claim = artifacts.require("./Claim.sol");
+const Tara = artifacts.require('./Tara.sol');
+const Claim = artifacts.require('./Claim.sol');
 
 module.exports = async (deployer, network, accounts) => {
   const walletAddress = accounts[0],
     trustedAddress = accounts[1];
 
-  await deployer.deploy(Tara, 1000000000);
+  await deployer.deploy(Tara, "10000000000000000000000000000");
   let token = await Tara.deployed();
   let balance = await token.balanceOf(walletAddress);
 
--- truffle-config.js
+++ truffle-config.js
@@ -18,15 +18,14 @@
  *
  */
 
-require("dotenv").config();
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const wallet = (arguments) => new HDWalletProvider(arguments);
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = process.env.MNEMONIC.toString().trim();
 
 module.exports = {
   /**
@@ -47,9 +46,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545,        // Standard Ethereum port (default: none)
-      network_id: "*",   // Any network (default: none)
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
     },
     // Another network with more advanced options...
     // advanced: {
@@ -62,14 +61,21 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    taraxa: {
+      host: '127.0.0.1',
+      port: 7777,
+      network_id: '55555',
+      confirmations: 0,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -86,7 +92,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "v0.7.6", // Fetch exact version from solc-bin (default: truffle's version)
+      version: 'v0.7.6', // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -98,5 +104,5 @@ module.exports = {
     },
   },
 
-  plugins: ["solidity-coverage"],
+  plugins: ['solidity-coverage'],
 };
