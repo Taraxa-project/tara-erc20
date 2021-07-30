db8dd72697deb860c988bfd9b2f4f0b368bdae30
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-07-30 12:53:56 +0300

    fix(multisend): USDT compatibility

--- contracts/Multisend.sol
+++ contracts/Multisend.sol
@@ -3,7 +3,35 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import '@openzeppelin/contracts/math/SafeMath.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+abstract contract ERC20Basic {
+    function totalSupply() public virtual returns (uint256);
+
+    function balanceOf(address who) public virtual returns (uint256);
+
+    function transfer(address to, uint256 value) public virtual;
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+abstract contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) public virtual returns (uint256);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public virtual;
+
+    function approve(address spender, uint256 value) public virtual;
+}
 
 /**
  * @dev Taraxa Multisend Contract
@@ -30,11 +58,11 @@ contract Multisend {
         );
 
         uint256 total = 0;
-        IERC20 erc20token = IERC20(token);
+        ERC20 erc20token = ERC20(token);
         uint8 i = 0;
         for (i; i < _recipients.length; i++) {
             erc20token.transferFrom(msg.sender, _recipients[i], _amounts[i]);
-            total += _amounts[i];
+            total = total.add(_amounts[i]);
         }
         emit TokensSent(total, token);
     }
