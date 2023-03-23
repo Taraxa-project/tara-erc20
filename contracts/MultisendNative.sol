03ec581a5fb0324af7911e79e41c4f5864cbb2c5
Elod23 <vargaelod23@gmail.com>
2023-03-23 21:39:33 +0200

    implement native multisend

---
+++ contracts/MultisendNative.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+/**
+ * @dev Taraxa Multisend Contract
+ *
+ * The Multisend Contract is used to do multiple native token transfers in the same transaction
+ */
+contract MultisendNative {
+    /**
+     * @dev fallback receiver
+     */
+    receive() external payable {}
+
+    /**
+     * @dev Transfers the tokens from the method caller to the participant.
+     * @return (lastSentIndex, totalSent) -> the index of the last sent and the total value.
+     * The method doesn't restrict the sender to strictly be abel to fill all the transactions
+     * as it will simply end the loop once there isn't enough balance for the next transfer.
+     *
+     * Emits a {TokensSent} event.
+     */
+    function multisendToken(address payable[] calldata _recipients, uint256[] calldata _amounts)
+        public
+        payable
+        returns (uint8, uint256)
+    {
+        require(_recipients.length <= 200, 'Multisend: max transfers per tx exceeded');
+        require(
+            _recipients.length == _amounts.length,
+            'Multisend: contributors and balances have different sizes'
+        );
+        require(msg.value >= _amounts[0], "Multisend: Sent value doesn't cover first transaction");
+
+        uint256 total = 0;
+        uint8 i = 0;
+        for (i; i < _recipients.length; ++i) {
+            if ((msg.value - total) >= _amounts[i]) {
+                _recipients[i].transfer(_amounts[i]);
+                total = total + _amounts[i];
+            } else {
+                if (msg.value > total) {
+                    payable(msg.sender).transfer(msg.value - total);
+                    emit SentBack(msg.value - total);
+                }
+                break;
+            }
+        }
+        emit TokensSent(i, total);
+        return (i, total);
+    }
+
+    /**
+     * @dev Emitted after all the tokens have been transfered.
+     */
+    event TokensSent(uint8 lastSentIndex, uint256 total);
+
+    /**
+     * @dev Emitted in case there are some tokens sent back.
+     */
+    event SentBack(uint256 amount);
+}
---
+++ test/MultisendNative.test.js
@@ -0,0 +1,194 @@
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
+
+const Multisend = artifacts.require('./MultisendNative.sol'),
+  BigNumber = web3.BigNumber,
+  { expectRevert } = require('@openzeppelin/test-helpers');
+
+require('chai').use(require('chai-as-promised')).use(require('chai-bignumber')(BigNumber)).should();
+
+contract('Multisend', (accounts) => {
+  beforeEach(async function () {
+    this.contract = await Multisend.new();
+  });
+
+  it('transfers tokens if inputs are valid', async function () {
+    let recipients = [];
+    let amounts = [];
+    for (let i = 0; i < 10; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push(123 + i);
+    }
+
+    const total = amounts.reduce((a, b) => {
+      return a + b;
+    }, 0);
+
+    const tx = await this.contract.multisendToken(recipients, amounts, {
+      from: accounts[0],
+      value: total,
+    });
+
+    for (let i = 0; i < 10; i++) {
+      const recipient = recipients[i];
+      const balance = await web3.eth.getBalance(recipient);
+      balance
+        .toString()
+        .should.be.bignumber.equal(
+          web3.utils.toBN(`${123 + i}`).toString(),
+          `Wrong recipient balace - : ${web3.utils.toBN(`${123 + i}`).toString()}`
+        );
+    }
+
+    const event = tx.logs.find((log) => log.event === 'TokensSent');
+    assert.exists(event, 'TokensSent event not found');
+    event.args.total.toString().should.equal(total.toString(), 'Total value mismatch');
+    event.args.lastSentIndex.toString().should.equal('10', 'Total value mismatch');
+  });
+
+  it('throws if value doesnt cover first tx', async function () {
+    let recipients = [];
+    let amounts = [];
+    for (let i = 0; i < 10; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push(123 + i);
+    }
+
+    await expectRevert(
+      this.contract.multisendToken(recipients, amounts, {
+        from: accounts[1],
+        value: 0,
+      }),
+      "VM Exception while processing transaction: revert Multisend: Sent value doesn't cover first transaction"
+    );
+  });
+
+  it('throws if number of recipients exceeds max number of txs', async function () {
+    let recipients = [];
+    let amounts = [];
+    for (let i = 0; i < 210; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push('0');
+    }
+
+    const total = amounts.reduce((a, b) => {
+      return a + b;
+    }, 0);
+
+    await expectRevert(
+      this.contract.multisendToken(recipients, amounts, {
+        from: accounts[1],
+        value: total,
+      }),
+      'Multisend: max transfers per tx exceeded'
+    );
+  });
+
+  it('throws if number of recipients is not the same as the number of amounts', async function () {
+    let recipients = [];
+    let amounts = [];
+
+    for (let i = 0; i < 10; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push('0');
+    }
+
+    amounts.pop();
+
+    const total = amounts.reduce((a, b) => {
+      return a + b;
+    }, 0);
+
+    await expectRevert(
+      this.contract.multisendToken(recipients, amounts, {
+        from: accounts[2],
+        value: total,
+      }),
+      'VM Exception while processing transaction: revert Multisend: contributors and balances have different sizes'
+    );
+  });
+
+  it('transfers tokens if inputs are valid until value runs out', async function () {
+    let recipients = [];
+    let amounts = [];
+    for (let i = 0; i < 10; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push(123 + i);
+    }
+
+    const total = amounts.reduce((a, b) => {
+      return a + b;
+    }, 0);
+
+    const totalWithoutLast = total - amounts[amounts.length - 1];
+    const tx = await this.contract.multisendToken(recipients, amounts, {
+      from: accounts[3],
+      value: totalWithoutLast,
+    });
+
+    for (let i = 0; i < 9; i++) {
+      const recipient = recipients[i];
+      const balance = await web3.eth.getBalance(recipient);
+      balance
+        .toString()
+        .should.be.bignumber.equal(
+          web3.utils.toBN(`${123 + i}`).toString(),
+          `Wrong recipient balace - : ${web3.utils.toBN(`${123 + i}`).toString()}`
+        );
+    }
+
+    const event = tx.logs.find((log) => log.event === 'TokensSent');
+    assert.exists(event, 'TokensSent event not found');
+    event.args.total.toString().should.equal(totalWithoutLast.toString(), 'Total value mismatch');
+    event.args.lastSentIndex.toString().should.equal('9', 'Total value mismatch');
+  });
+
+  it('transfers tokens if inputs are valid until value runs out - then sends back remaining balance', async function () {
+    let recipients = [];
+    let amounts = [];
+    for (let i = 0; i < 10; i++) {
+      const account = web3.eth.accounts.create();
+      recipients.push(account.address);
+      amounts.push(123 + i);
+    }
+
+    const total = amounts.reduce((a, b) => {
+      return a + b;
+    }, 0);
+
+    const totalWithoutLastPlusSome =
+      total - amounts[amounts.length - 1] + amounts[amounts.length - 1] / 2;
+    const totalWithoutLast = total - amounts[amounts.length - 1];
+    const difference = totalWithoutLastPlusSome - totalWithoutLast;
+    const tx = await this.contract.multisendToken(recipients, amounts, {
+      from: accounts[3],
+      value: totalWithoutLastPlusSome,
+    });
+
+    for (let i = 0; i < 9; i++) {
+      const recipient = recipients[i];
+      const balance = await web3.eth.getBalance(recipient);
+      balance
+        .toString()
+        .should.be.bignumber.equal(
+          web3.utils.toBN(`${123 + i}`).toString(),
+          `Wrong recipient balace - : ${web3.utils.toBN(`${123 + i}`).toString()}`
+        );
+    }
+
+    const event = tx.logs.find((log) => log.event === 'TokensSent');
+    assert.exists(event, 'TokensSent event not found');
+    event.args.total.toString().should.equal(totalWithoutLast.toString(), 'Total value mismatch');
+    event.args.lastSentIndex.toString().should.equal('9', 'Total value mismatch');
+
+    const eventSentBack = tx.logs.find((log) => log.event === 'SentBack');
+    assert.exists(eventSentBack, 'SentBack event not found');
+    eventSentBack.args.amount
+      .toString()
+      .should.equal(difference.toString(), 'Sent back value mismatch');
+  });
+});
